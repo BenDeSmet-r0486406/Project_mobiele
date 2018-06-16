ee3fe10f0457b688a536b806310923a073d0d35e
Wouter Cypers <wouter.cypers@gmail.com>
2018-06-16 23:23:20 +0200

    Added buttons for navigation between the screens

--- Korgtastic/screens/HomeScreen.js
+++ Korgtastic/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../styles/Styles.js';
-import { StyleSheet, Text, View,TextInput,ScrollView,TouchableHighlight,Image, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, TextInput,ScrollView,TouchableHighlight,Image, ImageBackground} from 'react-native';
 
 export default class HomeScreen extends React.Component {
 
@@ -21,10 +21,20 @@ export default class HomeScreen extends React.Component {
     }
 
     render() {
-        const { navigate } = this.props.navigation
-        return(
-            <View>
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.HomeView}>
                 <Text style={styles.TestText}>TEST TESTT</Text>
+                <TouchableHighlight
+                    onPress={() => navigate('Game')}
+                    style={styles.Button}>
+                    <Text style={styles.ButtonText}>Start game</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    onPress={() => navigate('Questions')}
+                    style={styles.Button}>
+                    <Text style={styles.ButtonText}>View questions</Text>
+                </TouchableHighlight>
             </View>
         );
     }
--- Korgtastic/styles/Styles.js
+++ Korgtastic/styles/Styles.js
@@ -4,10 +4,27 @@ import { AppRegistry, Text, View } from 'react-native';
 
 var styles = StyleSheet.create({
     TestText:
-        {
-            color: 'black',
-            fontSize: 20,
-            alignSelf: 'center',
-        },
+    {
+        color: 'black',
+        fontSize: 20,
+        alignSelf: 'center',
+    },
+    HomeView: 
+    {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    Button:
+    {
+        height: 30,
+        backgroundColor: '#303030',
+        marginBottom: 10,
+    },
+    ButtonText:
+    {
+        color: 'white',
+        textAlign: 'center',
+        fontSize:  20,
+    },
 });
 module.exports = styles;
