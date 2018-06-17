a9f79b6a4ba18dc3dd6971d9ae6826464c3fa7e8
Wouter Cypers <wouter.cypers@gmail.com>
2018-06-17 16:39:06 +0200

    Removed the alert

--- Korgtastic/screens/GameScreen.js
+++ Korgtastic/screens/GameScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from '../styles/Styles.js';
 import { StyleSheet, Text, View,ScrollView, Button, TouchableHighlight} from 'react-native';
-import CustomButton from '../components/CustomButton.js';
 import opdrachtenData from '../opdrachten/Normal.json';
 
 export default class GameScreen extends React.Component {
@@ -21,13 +20,9 @@ export default class GameScreen extends React.Component {
         this.state = 
         {
             Opdracht : "First things first, everybody take a sip of your glass to get started",
-            Graad : 0
+            Graad : 1
         }
         this.randomOpdracht = this.randomOpdracht.bind(this);
-    }
-
-    componentDidMount(){
-        alert(opdrachtenData.graad1.length + " " + opdrachtenData.graad2.length  + " " + opdrachtenData.graad3.length + " " + opdrachtenData.graad4.length  + " " + opdrachtenData.graad5.length)
     }
 
     randomOpdracht(){
@@ -40,25 +35,25 @@ export default class GameScreen extends React.Component {
         console.log(rand);
 
         if(rand <= 30){
-           newOpdracht = opdrachtenData.graad1[Math.floor(Math.random() * opdrachtenData.graad1.length)];
-           this.setState({Graad : 1 });
-        }else if(rand > 30 && rand <= 55 ){
+            newOpdracht = opdrachtenData.graad1[Math.floor(Math.random() * opdrachtenData.graad1.length)];
+            this.setState({Graad : 1 });
+        } else if(rand > 30 && rand <= 55 ){
             newOpdracht = opdrachtenData.graad2[Math.floor(Math.random() * opdrachtenData.graad2.length)];
             this.setState({Graad : 2 });
-        }else if(rand > 55 && rand <= 75 ){
+        } else if(rand > 55 && rand <= 75 ){
             newOpdracht = opdrachtenData.graad3[Math.floor(Math.random() * opdrachtenData.graad3.length)];
             this.setState({Graad : 3 });
-        }else if(rand > 75 && rand <= 90 ){
+        } else if(rand > 75 && rand <= 90 ){
             newOpdracht = opdrachtenData.graad4[Math.floor(Math.random() * opdrachtenData.graad4.length)];
             this.setState({Graad : 4 });
-        }else if(rand > 90 && rand <= 100 ){
+        } else if(rand > 90 && rand <= 100 ){
             newOpdracht = opdrachtenData.graad5[Math.floor(Math.random() * opdrachtenData.graad5.length)];
             this.setState({Graad : 5 });
         }
 
         if(this.state.Opdracht == newOpdracht){
             this.randomOpdracht
-        }else{
+        } else {
             this.setState({Opdracht : newOpdracht});
         }
     }
@@ -68,17 +63,16 @@ export default class GameScreen extends React.Component {
             <View style={{flex: 1}}>
                 <View style={styles.QuestionView}>
                     <Text style={styles.QuestionText}>{this.state.Opdracht}</Text>
-
                 </View>
                 <View style={styles.ButtonsView}>
                     <TouchableHighlight
                         style={[styles.acceptButton]}
-                        onPress={this.randomOpdracht }>
+                        onPress={this.randomOpdracht}>
                         <Text style={styles.ButtonText}>DO IT</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
                         style={[styles.refuseButton]}
-                        onPress={this.randomOpdracht }>
+                        onPress={this.randomOpdracht}>
                         <Text style={styles.ButtonText}>DRINK {this.state.Graad} SIPS</Text>
                     </TouchableHighlight>
                 </View>
