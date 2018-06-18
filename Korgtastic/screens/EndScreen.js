dbbf9b0d7c4fe1ed0c78b03dc80b27f3093c5a43
Wouter Cypers <wouter.cypers@gmail.com>
2018-06-18 12:01:23 +0200

    Updated EndScreen

--- Korgtastic/screens/EndScreen.js
+++ Korgtastic/screens/EndScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../styles/Styles.js';
-import { Text, View, SectionList } from 'react-native';
+import { Text, View, FlatList, TouchableHighlight } from 'react-native';
 
 export default class EndScreen extends React.Component {
 
@@ -14,10 +14,67 @@ export default class EndScreen extends React.Component {
 
     });
 
+    constructor(props) {
+		super(props);
+        this.state = 
+        {
+            positions: [
+                {
+                    name: 'Trump',
+                    score: 150,
+                    position: 1,
+                },
+                {
+                    name: 'Ben',
+                    score: 50,
+                    position: 2,
+                },
+                {
+                    name: 'Wouter',
+                    score: 20,
+                    position: 3,
+                },
+                {
+                    name: 'Cedric',
+                    score: 20,
+                    position: 3,
+                },
+            ],
+        }
+    }
+
     render() {
+        let winner = this.state.positions[0];
+
         return(
-            <View>
-                <Text>Winner: </Text>
+
+            <View style={{flex: 1}}>
+                <View style={{flex: 1, padding: 20}}>
+                    <Text>Winner:</Text>
+                </View>
+                <View style={{flex: 9, padding: 20}}>
+                    <FlatList
+                        data={this.state.positions}
+                        keyExtractor={(item, index) => item.name}
+                        renderItem={({item}) =>
+                            <View style={{flexDirection: 'row', height: 50}}>
+                                <Text style={{flex: 1, fontSize: 18}}>{item.position}</Text>
+                                <Text style={{flex: 5, fontSize: 18}}>{item.name}</Text>
+                                <Text style={{flex: 2, fontSize: 18}}>{item.score}</Text>
+                            </View>
+                        }
+                    />
+                </View>
+                <View style={styles.buttonsView}>
+                    <TouchableHighlight
+                        style={styles.button}>
+                        <Text style={styles.buttonText}>PLAY AGAIN</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.button}>
+                        <Text style={styles.buttonText}>GO TO MENU</Text>
+                    </TouchableHighlight>
+                </View>
             </View>
         );
     }
