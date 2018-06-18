94edfc47b3da97cf18f9b19becd6958c534b94db
Wouter Cypers <wouter.cypers@gmail.com>
2018-06-18 11:05:23 +0200

    Moved code from EndScreen to ScoreScreen as this makes more sense, we can get the data for EndScreen from ScoreScreen

--- Korgtastic/screens/ScoreScreen.js
+++ Korgtastic/screens/ScoreScreen.js
@@ -17,7 +17,69 @@ export default class ScoreScreen extends React.Component {
     constructor(props) {
 		super(props);
         this.state = 
-        {}
+        {
+            players: [
+                {
+                    name: 'Ben',
+                    score: 50,
+                },
+                {
+                    name: 'Wouter',
+                    score: 20,
+                },
+                {
+                    name: 'Cedric',
+                    score: 20,
+                },
+                {
+                    name: 'Trump',
+                    score: 150,
+                },
+            ],
+            positions: [],
+        }
+    }
+
+    componentDidMount() {
+        this.sortPlayers();
+        this.calculatePositions();
+    }
+
+    sortPlayers = () => {
+        let players = this.state.players;
+
+        players.sort(function(a, b) {
+            return b.score - a.score;
+        });
+
+        this.setState({players});
+    }
+
+    calculatePositions = () => {
+        let players = this.state.players;
+        let positions = this.state.positions;
+
+        let position = 1;
+
+        positions.push({
+            name: players[0].name,
+            score: players[0].score,
+            position: position,
+        });
+
+        for(let i = 1; i < players.length; i++) {
+            if(players[i - 1].score > players[i].score) {
+                position++;
+            }
+
+            positions.push({
+                name: players[i].name,
+                score: players[i].score,
+                position: position,
+            });
+        }
+
+        console.log(positions);
     }
 
     render() {
