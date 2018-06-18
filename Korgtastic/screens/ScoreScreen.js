d640617689b3d1dd3955868b2a5832ec12ed0a8c
Wouter Cypers <wouter.cypers@gmail.com>
2018-06-18 11:32:36 +0200

    Layout & styling for the scoreboard

--- Korgtastic/screens/ScoreScreen.js
+++ Korgtastic/screens/ScoreScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from '../styles/Styles.js';
-import { Text, View, SectionList } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 
 export default class ScoreScreen extends React.Component {
 
     static navigationOptions = () => ({
-        title: 'ScoreScreen',
+        title: 'Scoreboard',
         headerTintColor: '#ffffff',
         headerStyle:
             {
@@ -84,8 +84,25 @@ export default class ScoreScreen extends React.Component {
 
     render() {
         return(
-            <View>
-                <Text>ScoreScreen</Text>
+            <View style={{flex: 1}}>
+                <View style={{flex: 1, flexDirection: 'row', backgroundColor: '#333', padding: 20}}>
+                    <Text style={{flex: 1, color: 'white', fontSize: 20}}>#</Text>
+                    <Text style={{flex: 5, color: 'white', fontSize: 20}}>Name</Text>
+                    <Text style={{flex: 2, color: 'white', fontSize: 20}}>Score</Text>
+                </View>
+                <View style={{flex: 20, padding: 20}}>
+                    <FlatList
+                        data={this.state.positions}
+                        keyExtractor={(item, index) => item.name}
+                        renderItem={({item}) =>
+                            <View style={{flexDirection: 'row', height: 50}}>
+                                <Text style={{flex: 1, fontSize: 18}}>{item.position}</Text>
+                                <Text style={{flex: 5, fontSize: 18}}>{item.name}</Text>
+                                <Text style={{flex: 2, fontSize: 18}}>{item.score}</Text>
+                            </View>
+                        }
+                    />
+                </View>
             </View>
         );
     }
